9073d6de11159b5e0689f75bbdae49a609b2aa8f
Katrín Ósk Hafsteinsdóttir <katrinh@vodafone.is>
2016-11-28 14:46:27 +0000

    RosaBrosa

--- MassaFlottaTestid/main.cpp
+++ MassaFlottaTestid/main.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "Hallo Heimur!!" << endl;
     cout << "Hallo Welt!!" << endl;
     cout << "Svaka steik!!" << endl;
+    cout << "Nu er eg ad baeta inni origin kata" << endl;
 
     return 0;
 }
