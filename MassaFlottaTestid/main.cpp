a93882de9b480a88d3bb798e1b11b34be63643aa
Hjördís Helga Jensdóttir <Hjrds Helga Jensdttir>
2016-11-29 09:33:36 +0000

    testa aftur

--- MassaFlottaTestid/main.cpp
+++ MassaFlottaTestid/main.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
+
     int number;
     cout << "Insert number" << endl;
     cin >> number;
-=======
+
     cout << "Hallo Heimur!!" << endl;
     cout << "Hallo Welt!!" << endl;
     cout << "Svaka steik!!" << endl;
     cout << "Nu er eg ad baeta inni origin kata" << endl;
+    cout << "Prufa ad baeta vid" << endl;
 
->>>>>>> 07473f0094c54e41633bbc3d6c5dd5d82ef1f772
+
     return 0;
 }
