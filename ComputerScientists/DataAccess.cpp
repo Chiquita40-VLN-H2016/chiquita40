0bce839895ff2c5c9548d9c140cbdc424d8114dd
Diemut Haberbusch <diemut@nordic.lan>
2016-11-29 15:19:43 +0000

    Diemut vara a fikta

--- ComputerScientists/DataAccess.cpp
+++ ComputerScientists/DataAccess.cpp
@@ -1,6 +1,44 @@
 #include "DataAccess.h"
+#include "Scientist.h"
+#include "ScientistService.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
 
 DataAccess::DataAccess()
 {
 
+}
+
+DataAccess::DataAccess(vector<Scientist> scientists)
+{
+    _scientists = scientists;
+}
+
+size_t DataAccess::size() const
+{
+    return _scientists.size();
+}
+
+void DataAccess::readScientist(Scientist& setScientist)
+{
+    ifstream document;
+    string word;
+
+    document.open("scientists.txt");
+    while(document >> word)
+    {
+        setScientist.getName();
+        setScientist.getGender();
+        setScientist.getBirthDate();
+        setScientist.getDeathDate();
+
+        _scientists.push_back(word);
+    }
+
+    document.close();
 }
--- ComputerScientists/DataAccess.h
+++ ComputerScientists/DataAccess.h
@@ -1,11 +1,24 @@
 #ifndef DATAACCESS_H
 #define DATAACCESS_H
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "Scientist.h"
+
+using namespace std;
 
 class DataAccess
 {
 public:
     DataAccess();
+    DataAccess(vector<Scientist> scientists);
+    size_t size() const;
+    void readScientist(Scientist& setScientist);
+
+private:
+    vector<Scientist> _scientists;
 };
 
 #endif // DATAACCESS_H
