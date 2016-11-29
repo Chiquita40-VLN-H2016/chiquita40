6d06542897f12220bc534297a2f2d9f1bdcafadb
einkria <kristintj15@ru.is>
2016-11-29 13:31:20 +0100

    scientist class first edition ready

--- ComputerScientists/Scientist.cpp
+++ ComputerScientists/Scientist.cpp
@@ -6,8 +6,24 @@ Scientist::Scientist()
 }
 Scientist::Scientist(string n, int bd, int dd, char g)
 {
-    name = n;
-    birthDate = bd;
-    deathDate = dd;
-    gender = g;
+    _name = n;
+    _birthDate = bd;
+    _deathDate = dd;
+    _gender = g;
+}
+string Scientist::getName() const
+{
+    return _name;
+}
+int Scientist::getBirthDate() const
+{
+    return _birthDate;
+}
+int Scientist::getDeathDate() const
+{
+    return _deathDate;
+}
+char Scientist::getGender() const
+{
+    return _gender;
 }
--- ComputerScientists/Scientist.h
+++ ComputerScientists/Scientist.h
@@ -2,17 +2,24 @@
 #define SCIENTIST_H
 #include <string>
 
+using namespace std;
+
 
 class Scientist
 {
 private:
-    string name;
-    int birthDate;
-    int deathDate;
-    char gender;
+    string _name;
+    int _birthDate;
+    int _deathDate;
+    char _gender;
 public:
     Scientist();
     Scientist(string n, int bd, int dd, char g);
+    string getName() const;
+    int getBirthDate() const;
+    int getDeathDate() const;
+    char getGender() const;
+
 };
 
 #endif // SCIENTIST_H
