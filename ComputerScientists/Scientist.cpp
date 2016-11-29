da7b556408cd4788eecf761cacc9462602df37ab
einkria <kristintj15@ru.is>
2016-11-29 12:57:13 +0100

    Private variables and constructor created

--- ComputerScientists/Scientist.cpp
+++ ComputerScientists/Scientist.cpp
@@ -1,6 +1,13 @@
-#include "scientist.h"
+#include "Scientist.h"
 
 Scientist::Scientist()
 {
 
+}
+Scientist::Scientist(string n, int bd, int dd, char g)
+{
+    name = n;
+    birthDate = bd;
+    deathDate = dd;
+    gender = g;
 }
--- ComputerScientists/Scientist.h
+++ ComputerScientists/Scientist.h
@@ -1,11 +1,18 @@
 #ifndef SCIENTIST_H
 #define SCIENTIST_H
+#include <string>
 
 
 class Scientist
 {
+private:
+    string name;
+    int birthDate;
+    int deathDate;
+    char gender;
 public:
     Scientist();
+    Scientist(string n, int bd, int dd, char g);
 };
 
 #endif // SCIENTIST_H
